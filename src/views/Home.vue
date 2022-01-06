8a1ec5237ef177e152a338b4dc6da1cedb772a12
Israel <israpalomoe@gmail.com>
2022-01-06 12:33:15 +0100

    First commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Form />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Form from "@/components/Form.vue";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: { Form },
 })
 export default class Home extends Vue {}
 </script>
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
